2fef954ee53d917fc3b2170fef39f5ac0df7a651
Ceddyp19 <ceddyp117@gmail.com>
2020-12-25 17:28:17 -0600

    trying to get setState to work properly

--- random-quote-machine/src/QuoteBox.js
+++ random-quote-machine/src/QuoteBox.js
@@ -5,26 +5,35 @@ import QuoteGenerator from './QuoteGenerator';
 export default class QuoteBox extends React.Component {
     constructor(props) {
         super(props)
-        this.state={quote: '', author: ''}
+        this.state = { text: '', author: '' }
         this.getQuote = this.getQuote.bind(this);
     }
 
-    getQuote() {
-        console.log('it works')
-    fetch("https://type.fit/api/quotes")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-}
 
-render(){
-    return (
-        <div id='quote-box' style={{ backgroundColor: 'red', display: 'inline-block' }}>
-            <QuoteGenerator getQuote={this.getQuote} author={this.state.author} text={this.state.text}/>
-        </div>
-    )
-}
+    getQuote() {
+   let randomQuote;
+        fetch("https://type.fit/api/quotes")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                // console.log(data);
+                 randomQuote = data[Math.floor(Math.random() * 1650)]
+                console.log('random', randomQuote.author)
+                // text = randomQuote.text;
+                // author = randomQuote.author;
+            });
+            console.log('fsafds', randomQuote)
+        this.setState({ text: randomQuote.text, author: randomQuote.author })
+        // console.log('quote', this.state.text, this.state.author)
+    }
+   
+
+    render() {
+        return (
+            <div id='quote-box' style={{ backgroundColor: 'red', display: 'inline-block' }}>
+                <QuoteGenerator getQuote={this.getQuote} author={this.state.author} text={this.state.text} />
+            </div>
+        )
+    }
 }
--- random-quote-machine/src/QuoteGenerator.js
+++ random-quote-machine/src/QuoteGenerator.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 const QuoteGenerator = (props) => {
+    console.log('quote',props.text, props.author)
     return (
         <div>
-            <blockquote id='text'> Quote goes here</blockquote>
-            <span id='author'>Author goes here</span>
+            <blockquote id='text'>{props.text}</blockquote>
+            <span id='author'>{props.author}</span>
             <button id='new-quote' onClick={props.getQuote}>New Quote</button>
             <a href='#' id='tweet-quote'>Tweet</a>
         </div>
