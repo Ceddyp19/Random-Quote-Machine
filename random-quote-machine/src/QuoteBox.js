32462bed2d8a576911208f0117faa7b2350666a0
Ceddyp19 <ceddyp117@gmail.com>
2020-12-25 17:57:47 -0600

    used componentDidMount, state is setting properly

--- random-quote-machine/src/QuoteBox.js
+++ random-quote-machine/src/QuoteBox.js
@@ -1,33 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './QuoteBox.css'
 import QuoteGenerator from './QuoteGenerator';
 
 export default class QuoteBox extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { text: '', author: '' }
+        this.state = {
+            text: '',
+            author: '', 
+            quotes: []
+        }
         this.getQuote = this.getQuote.bind(this);
+    }
+
+    componentDidMount = () => {
+        fetch("https://type.fit/api/quotes")
+            .then(resp => resp.json())
+            .then(quotes => this.setState({ quotes }))
     }
 
 
     getQuote() {
-   let randomQuote;
-        fetch("https://type.fit/api/quotes")
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // console.log(data);
-                 randomQuote = data[Math.floor(Math.random() * 1650)]
-                console.log('random', randomQuote.author)
-                // text = randomQuote.text;
-                // author = randomQuote.author;
-            });
-            console.log('fsafds', randomQuote)
-        this.setState({ text: randomQuote.text, author: randomQuote.author })
+        let quotes = this.state.quotes
+        let randomQuote = quotes[Math.floor(Math.random() * 1644)]
+        this.setState({
+            author: randomQuote.author,
+            text: randomQuote.text
+        })
+
+        // var randomQuote = {};
+        // fetch("https://type.fit/api/quotes")
+        //     .then(function (response) {
+        //         return response.json();
+        //     })
+        //     .then(function (data) {
+        //         // console.log(data);
+        //         randomQuote = data[Math.floor(Math.random() * 1644)]
+        //         console.log('random', randomQuote.author)
+        //         this.state.text = randomQuote.text;
+        //         this.state.author = randomQuote.author;
+        //     });
+        // console.log(this.state.quotes)
+        // this.setState({ text: randomQuote.text, author: randomQuote.author })
         // console.log('quote', this.state.text, this.state.author)
     }
-   
+
 
     render() {
         return (
